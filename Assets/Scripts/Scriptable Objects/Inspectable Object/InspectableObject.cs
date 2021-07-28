0c76614658ecd569245dd1a16144bab20ff69d38
Thev2Andy <ciprian.andrei002@gmail.com>
2021-07-28 21:35:00 +0300

    Update default inspectable key.

---
+++ Assets/Scripts/Scriptable Objects/Inspectable Object/InspectableObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InspectableData", menuName = "ScriptableObjects/Object Data/Inspectable Object Data")]
+public class InspectableObject : ScriptableObject {
+
+	public string ItemName;
+	public string ItemType;
+	public string ItemDescription;
+	public Sprite ItemImage;
+	public KeyCode InspectKey = KeyCode.Y;
+}
